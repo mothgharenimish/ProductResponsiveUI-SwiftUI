f899054282834afab9bdd121a60c8c6ebf0cef8b
mothgharenimish <mothgharenimish0@gmail.com>
2025-05-04 17:45:44 +0530

    Xcoded Updated Changes

---
+++ ProductResponsiveUI-SwiftUI/Model/OffersModel.swift
@@ -0,0 +1,8 @@
+//
+//  OffersModel.swift
+//  ProductResponsiveUI-SwiftUI
+//
+//  Created by Nimish Mothghare on 04/05/25.
+//
+
+import Foundation
---
+++ ProductResponsiveUI-SwiftUI/View/OfferCard.swift
@@ -0,0 +1,18 @@
+//
+//  OfferCard.swift
+//  ProductResponsiveUI-SwiftUI
+//
+//  Created by Nimish Mothghare on 04/05/25.
+//
+
+import SwiftUI
+
+struct OfferCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OfferCard()
+}
